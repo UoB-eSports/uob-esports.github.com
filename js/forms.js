51904df3c420e4d0ec488e073089bb0eaefdc951
aaunel <yann@aaunel.com>
2014-11-09 23:32:52 +0000

    Added deep-linking to form tabs, and instated shortcuts on homepage

--- js/forms.js
+++ js/forms.js
@@ -1,5 +1,14 @@
-(function($) {
+(function(window, document, $) {
 
+	// Javascript to enable link to tab
+    if (document.location.toString().match('#')) {
+        $('#tabs a[href=#'+document.location.toString().split('#')[1]+']').tab('show');
+    }
+
+    // Change hash for page-reload
+    $('#tabs a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        window.location.hash = e.target.hash;
+        window.scrollTo(0, 0);
+    });
 	
-	
-})(jQuery);
+})(window, document, jQuery);
